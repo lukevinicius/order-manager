da19a52254ae65ba956cd9c84f6f39007a14d6d3
Lucas vinicius alencar alves <lucasviniciusaalves@gmail.com>
2024-11-16 17:05:51 -0300

    update: logo text

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,8 @@ export function Logo() {
   return (
     <Link href="/">
       {/* <Image src="{logo}" alt="Logo" width={128} height={128} /> */}
-      Logomarca
+      <p className="text-2xl font-semibold text-emerald-500">Order</p>
+      <p className="text-2xl font-semibold text-zinc-200">Manager</p>
     </Link>
   )
 }
