0c4024dee53a5ac3fa2313138bfc7c6607121e0b
Lucas vinicius alencar alves <lucasviniciusaalves@gmail.com>
2024-11-17 17:34:31 -0300

    update: cols in list orders

--- src/app/back-office/orders/list-orders.tsx
+++ src/app/back-office/orders/list-orders.tsx
@@ -9,44 +9,42 @@ export async function ListOrders() {
   return (
     <div className="space-y-4">
       <p className="font-semibold">Pedidos em aberto</p>
-      <div className="rounded-xl bg-zinc-800 p-4">
-        <div className="grid w-full gap-2 md:grid-cols-3">
-          {openOrders.map((order) => (
-            <div
-              key={order.id}
-              className="w-full space-y-2 rounded-xl bg-zinc-700 p-4"
-            >
-              <p>Cliente: {order.client}</p>
-              <p>
-                Status:{' '}
-                <span
-                  className={`font-semibold ${
-                    order.status === OrderStatus.OPEN
-                      ? 'text-green-500'
-                      : 'text-red-500'
-                  }`}
-                >
-                  {order.status}
-                </span>
-              </p>
-              <p className="font-semibold">
-                Total:{' '}
-                {order.total.toLocaleString('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                })}
-              </p>
-              <UpdateOrderDialog orderId={order.id} />
-              {order.status === OrderStatus.OPEN && (
-                <CloseOrderButton orderId={order.id} />
-              )}
-            </div>
-          ))}
-        </div>
+      <div className="grid w-full gap-2 md:grid-cols-3 lg:grid-cols-4">
+        {openOrders.map((order) => (
+          <div
+            key={order.id}
+            className="w-full space-y-2 rounded-xl bg-zinc-700 p-4"
+          >
+            <p>Cliente: {order.client}</p>
+            <p>
+              Status:{' '}
+              <span
+                className={`font-semibold ${
+                  order.status === OrderStatus.OPEN
+                    ? 'text-green-500'
+                    : 'text-red-500'
+                }`}
+              >
+                {order.status}
+              </span>
+            </p>
+            <p className="font-semibold">
+              Total:{' '}
+              {order.total.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              })}
+            </p>
+            <UpdateOrderDialog orderId={order.id} />
+            {order.status === OrderStatus.OPEN && (
+              <CloseOrderButton orderId={order.id} />
+            )}
+          </div>
+        ))}
       </div>
 
       <p className="font-semibold">Pedidos fechados</p>
-      <div className="grid w-full gap-2 md:grid-cols-3">
+      <div className="grid w-full gap-2 md:grid-cols-3 lg:grid-cols-4">
         {closedOrders.map((order) => (
           <div
             key={order.id}
